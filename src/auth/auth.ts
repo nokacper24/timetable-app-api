ba59ae7c599b4813f8893401ad84f803ec6c69e1
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-06-27 14:28:52 +0200

    feat: :sparkles: use auth middleware on all /priv requests, throw error when cookie invalid

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,52 +1,50 @@
 import { Context, Hono } from "hono";
-import { getCookie, setCookie } from 'hono/cookie'
+import { getCookie } from "hono/cookie";
 import { User } from "../models";
 import { MyError } from "../my_error";
 import { COOKIE_NAME } from "..";
 
-export const auth_middleware = async (c: Context, next: () => Promise<void>) => {
-    
-    let user: User;
+export const auth_middleware = async (
+  c: Context,
+  next: () => Promise<void>
+) => {
+  let user: User | undefined;
 
-    const cookie = getCookie(c, COOKIE_NAME)
+  const cookie = getCookie(c, COOKIE_NAME);
 
-    if (cookie) {
-        user = await c.env.DB.prepare(
-            `SELECT username, passwordhash, role
+  if (cookie) {
+    user = await c.env.DB.prepare(
+      `SELECT username, role
             FROM users, sessions
             WHERE session_secret = ? AND users.id = sessions.userid`
-        ).bind(cookie).first();
-    } else {
-        throw new MyError('Not logged in', 401);
-    }
-    
+    )
+      .bind(cookie)
+      .first();
+  } else {
+    throw new MyError("Not logged in", 401);
+  }
+  if (!user) {
+    throw new MyError("Bad cookie", 401);
+  }
 
-    c.set('user', user);
-    
-    await next();
+  c.set("user", user);
 
+  await next();
 };
 
 type ContextLoggedIn = {
-    Bindings: {
-      DB: D1Database;
-    };
-    Variables: {
-      user: User;
-    };
+  Bindings: {
+    DB: D1Database;
   };
+  Variables: {
+    user: User;
+  };
+};
 
 export const priv = new Hono<ContextLoggedIn>();
-// priv.use('*', auth_middleware);
+priv.use("*", auth_middleware);
 
-
-
-priv.get('/me', auth_middleware, async (c) => {
-    const user = c.get('user');
-    return c.json(user);
-  });
-
-priv.get('/setcookie', async (c) => {
-  setCookie(c, COOKIE_NAME, 'sessionid');
-  return c.json({ message: 'cookie set' });
+priv.get("/me", async (c) => {
+  const user = c.get("user");
+  return c.json(user);
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { MyError } from "./my_error";
 import { login_form } from "./auth/login_form";
 import { priv } from "./auth/auth";
+import { setCookie } from "hono/cookie";
 
 export const COOKIE_NAME = 'session';
 
@@ -40,6 +41,10 @@ app.get("/login-form", async (c) => {
   return c.html(login_form);
 });
 
+app.get("/setcookie", async (c) => { // TODO remove, testing only
+  setCookie(c, COOKIE_NAME, "sessionid");
+  return c.json({ message: "cookie set" });
+});
 
 app.route('/priv', priv); // add all routes from /priv to app
 export default app;
